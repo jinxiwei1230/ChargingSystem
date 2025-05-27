782405606dfed0e0d85d35ee93206d37f2359da1
jinxiwei1230 <3492548835@qq.com>
2025-05-27 14:28:06 +0800

    删除mapper的注解，使用xml

--- src/main/java/com/online/chargingSystem/mapper/ChargingRequestMapper.java
+++ src/main/java/com/online/chargingSystem/mapper/ChargingRequestMapper.java
@@ -7,16 +7,12 @@ import java.util.List;
 
 @Mapper
 public interface ChargingRequestMapper {
-    @Select("SELECT * FROM charging_requests WHERE status = #{status}")
     List<ChargingRequest> findByStatus(String status);
 
-    @Select("SELECT * FROM charging_requests WHERE request_mode = #{mode} AND status = #{status}")
     List<ChargingRequest> findByRequestModeAndStatus(String mode, String status);
 
-    @Select("SELECT * FROM charging_requests WHERE car_id = #{carId} AND status = #{status}")
     ChargingRequest findByCarIdAndStatus(String carId, String status);
 
-    @Update("UPDATE charging_requests SET pile_id = #{pileId}, status = #{status} WHERE id = #{id}")
     int update(ChargingRequest request);
 
     // 插入充电请求
