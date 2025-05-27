050809df1f90721885543caac6de1f56fbf0a017
jinxiwei1230 <3492548835@qq.com>
2025-05-27 14:28:06 +0800

    删除mapper的注解，使用xml

--- src/main/java/com/online/chargingSystem/mapper/ChargingQueueMapper.java
+++ src/main/java/com/online/chargingSystem/mapper/ChargingQueueMapper.java
@@ -6,24 +6,16 @@ import java.util.List;
 
 @Mapper
 public interface ChargingQueueMapper {
-    @Select("SELECT * FROM charging_queues WHERE pile_id = #{pileId}")
     ChargingQueue findByPileId(String pileId);
 
-    @Select("SELECT * FROM charging_queues WHERE queue_type = #{queueType}")
     List<ChargingQueue> findByQueueType(String queueType);
 
-    @Select("SELECT * FROM charging_queues WHERE car_id = #{carId}")
     List<ChargingQueue> findByCarId(String carId);
 
-    @Select("SELECT * FROM charging_queues WHERE queue_type = #{queueType} AND status = #{status}")
     List<ChargingQueue> findByQueueTypeAndStatus(String queueType, String status);
 
-    @Select("SELECT * FROM charging_queues WHERE id = #{id}")
     ChargingQueue findById(Long id);
 
-    @Insert("INSERT INTO charging_queues (car_id, request_mode, request_amount, queue_type, status, queue_time) " +
-            "VALUES (#{carId}, #{requestMode}, #{requestAmount}, #{queueType}, #{status}, #{queueTime})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
     int insert(ChargingQueue queue);
 
     // 更新队列记录
