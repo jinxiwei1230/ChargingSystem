441c27478e91aed32fdd4031b86f58ed60cf6535
Revirado061 <19307824027@163.com>
2025-05-31 21:36:45 +0800

    完善注册逻辑，添加电池容量属性

--- src/main/java/com/online/chargingSystem/entity/enums/RequestStatus.java
+++ src/main/java/com/online/chargingSystem/entity/enums/RequestStatus.java
@@ -1,7 +1,12 @@
 package com.online.chargingSystem.entity.enums;
 
+import lombok.Getter;
+
+//充电请求的状态
+@Getter
 public enum RequestStatus {
-    WAITING("等待中"),
+    WAITING_IN_WAITING_AREA("在等待区等待"),
+    WAITING_IN_CHARGING_AREA("在充电区等待"),
     CHARGING("充电中"),
     COMPLETED("已完成"),
     CANCELED("已取消");
@@ -12,7 +17,4 @@ public enum RequestStatus {
         this.description = description;
     }
 
-    public String getDescription() {
-        return description;
-    }
-} 
+}
