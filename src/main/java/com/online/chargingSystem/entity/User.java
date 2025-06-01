cc4dc246559408aadc9fbc5b8adbe118302643a1
Revirado061 <19307824027@163.com>
2025-06-01 23:25:01 +0800

    添加排队号码生成逻辑

--- src/main/java/com/online/chargingSystem/entity/User.java
+++ src/main/java/com/online/chargingSystem/entity/User.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class User {
     private Long id;
     private String username;
+    private String password;
     // 车牌号
     private String carNumber;
     // 车辆电池总容量(度)
     private Double batteryCapacity;
-    private String password;
 }
